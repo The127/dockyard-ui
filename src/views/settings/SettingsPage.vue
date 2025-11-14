8c3d9db1a5aad85c6f5a1b95b5b8fa3ce53a383b
karo <karolin.kostial@gmail.com>
2025-11-14 21:06:13 +0100

    Integrate `PageLayout` and `PageHeader` into `SettingsPage` for consistent layout

--- src/views/settings/SettingsPage.vue
+++ src/views/settings/SettingsPage.vue
@@ -1,9 +1,14 @@
 <script setup>
 
+import PageLayout from "../../components/PageLayout.vue";
+import PageHeader from "../../components/PageHeader.vue";
 </script>
 
 <template>
-  Settings
+  <PageLayout>
+    <PageHeader title="Settings" subtitle="Configure your personal dockyard settings."/>
+    TODO
+  </PageLayout>
 </template>
 
 <style scoped>
