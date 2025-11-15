58d397d6e27ef18621f79ed2069b77238f996edf
karo <karolin.kostial@gmail.com>
2025-11-15 11:30:13 +0100

    Add repositories tab to project details page
    Integrate `TabLayout` and `TabPage` components in `ProjectDetails.vue` to enable tab navigation. Add a placeholder `RepositoriesTab.vue` component for repositories management.

--- src/views/projects/details/ProjectDetails.vue
+++ src/views/projects/details/ProjectDetails.vue
@@ -2,11 +2,22 @@
 
 import PageLayout from "../../../components/PageLayout.vue";
 import PageHeader from "../../../components/PageHeader.vue";
+import TabLayout from "../../../components/TabLayout.vue";
+import TabPage from "../../../components/TabPage.vue";
+import RepositoriesTab from "./RepositoriesTab.vue";
 </script>
 
 <template>
   <PageLayout>
     <PageHeader title="Details" subtitle="Manage the stuff."/>
+    <TabLayout>
+      <TabPage title="Repositories" name="repos">
+        <RepositoriesTab/>
+      </TabPage>
+      <TabPage title="Settings" name="settings">
+        TODO
+      </TabPage>
+    </TabLayout>
   </PageLayout>
 </template>
 
---
+++ src/views/projects/details/RepositoriesTab.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  TODO
+</template>
+
+<style scoped>
+
+</style>
