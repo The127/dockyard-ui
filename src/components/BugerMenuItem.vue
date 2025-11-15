6a4a4faf2142ac0e2add97f02924ef6e39b7f31d
karo <karolin.kostial@gmail.com>
2025-11-15 11:03:17 +0100

    Fix `BurgerMenuItem` active class to use `active-class` instead of `exact-active-class`

--- src/components/BugerMenuItem.vue
+++ src/components/BugerMenuItem.vue
@@ -12,7 +12,7 @@ defineProps({
   <router-link
       :to="to"
       class="p-2 rounded-md flex flex-row gap-3 hover:inset-shadow-2xs not-dark:hover:bg-slate-200 dark:hover:bg-slate-700 hover:shadow-none"
-      exact-active-class="not-dark:bg-offwhite dark:bg-slate-900 shadow-md not-dark:text-cyan-800 dark:text-cyan-200"
+      active-class="not-dark:bg-offwhite dark:bg-slate-900 shadow-md not-dark:text-cyan-800 dark:text-cyan-200"
   >
     <slot/>
   </router-link>
