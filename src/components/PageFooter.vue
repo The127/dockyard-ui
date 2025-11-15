aca5de453073a93f875faa3493b0f0fb18c644e0
karo <karolin.kostial@gmail.com>
2025-11-15 19:23:04 +0100

    Add `PageFooter` component with Dockyard attribution and integrate it into `PageLayout`

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+import DockyardIcon from "./DockyardIcon.vue";
+</script>
+
+<template>
+  <div class="border-t not-dark:border-t-slate-200 dark:border-t-slate-600 py-4">
+    <div class="flex flex-row justify-end gap-1 not-dark:text-slate-500 dark:text-slate-400">
+      Powered by
+      <a
+          href="https://github.com/The127/dockyard"
+          class="flex flex-row gap-1 items-center not-dark:text-cyan-600 dark:text-cyan-400 hover:not-dark:text-cyan-800 hover:dark:text-cyan-300"
+      >
+        <DockyardIcon class="size-4"/> dockyard
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -1,12 +1,14 @@
 <script setup>
 
 import BreadcrumbNavigation from "./BreadcrumbNavigation.vue";
+import PageFooter from "./PageFooter.vue";
 </script>
 
 <template>
   <div class="flex flex-col gap-4 px-2">
     <BreadcrumbNavigation/>
     <slot/>
+    <PageFooter/>
   </div>
 </template>
 
