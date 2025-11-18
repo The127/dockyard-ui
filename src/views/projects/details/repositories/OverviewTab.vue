8e495b72902583d331fc7854031ab63717130ada
karo <karolin.kostial@gmail.com>
2025-11-18 22:37:52 +0100

    Refactor `OverviewTab` to enhance readability and data handling. Update `useGetRepositoryReadmeQuery` bindings, improve template formatting, and adjust variable deserialization logic. Add debug logging for `contentBase64`.

--- src/views/projects/details/repositories/OverviewTab.vue
+++ src/views/projects/details/repositories/OverviewTab.vue
@@ -7,7 +7,7 @@ import {MdEditor, MdPreview} from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
 import {computed, markRaw, ref, useId} from "vue";
 import ButtonComponent from "../../../../components/ButtonComponent.vue";
-import { Pencil } from "lucide-vue-next";
+import {Pencil} from "lucide-vue-next";
 import {useDark} from "@vueuse/core";
 
 const route = useRoute()
@@ -27,7 +27,7 @@ defineProps({
 const editing = ref(false)
 const content = ref('#test')
 
-const {readmeData} = useGetRepositoryReadmeQuery(
+const {data: readmeData} = useGetRepositoryReadmeQuery(
     route.params.tenant,
     route.params.project,
     route.params.repository,
@@ -62,7 +62,7 @@ const onCancel = () => {
 const onEdit = () => {
   if (!readmeData?.value?.contentBase64) {
     content.value = ""
-  }else{
+  } else {
     content.value = decodedContent(readmeData.value.contentBase64)
   }
 
@@ -70,6 +70,7 @@ const onEdit = () => {
 }
 
 const decodedContent = (contentBase64) => {
+  console.log(contentBase64)
   return new TextDecoder().decode(Uint8Array.fromBase64(contentBase64))
 }
 
@@ -80,18 +81,23 @@ const editorTheme = computed(() => {
 </script>
 
 <template>
-  TODO: show data: {{ data }}
+  <span>TODO: show data: {{ data }}</span>
+  <span>asd: {{ readmeData }}</span>
   <template v-if="editing">
-    <MdEditor  class="rounded-md" :id="editorId" v-model="content" language="en-US" :theme="editorTheme"/>
+    <MdEditor class="rounded-md" :id="editorId" v-model="content" language="en-US" :theme="editorTheme"/>
     <div class="flex flex-row gap-2">
       <ButtonComponent size="sm" text="Save" @click="onSaveReadme"/>
       <ButtonComponent size="sm" variant="secondary" text="Cancel" @click="onCancel"/>
     </div>
   </template>
-  <template v-else-if="readmeData
- ?.contentBase64">
-    <MdPreview class="rounded-md px-4" :id="viewerId" :modelValue="decodedContent(readmeData
-   .contentBase64)" language="en-US" :theme="editorTheme"/>
+  <template v-else-if="readmeData?.contentBase64">
+    <MdPreview
+        class="rounded-md px-4"
+        :id="viewerId"
+        :modelValue="decodedContent(readmeData.contentBase64)"
+        language="en-US"
+        :theme="editorTheme"
+    />
     <ButtonComponent variant="link" size="sm" text="Edit" @click="onEdit">
       <template #adornment>
         <Pencil class="size-4"/>
@@ -99,7 +105,9 @@ const editorTheme = computed(() => {
     </ButtonComponent>
   </template>
   <div v-else class="flex flex-col gap-2 items-center">
-    <span class="not-dark:text-slate-500 dark:text-slate-400">This repository does not have a README yet.</span>
+    <span class="not-dark:text-slate-500 dark:text-slate-400">
+      This repository does not have a README yet.
+    </span>
     <ButtonComponent variant="link" size="sm" text="Create a README" @click="onEdit">
       <template #adornment>
         <Pencil class="size-4"/>
