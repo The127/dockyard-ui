8b20fd433948737019fc704c93cd95f69fc7a017
karo <karolin.kostial@gmail.com>
2025-11-19 19:39:10 +0100

    Add `PatsTab` and integrate it within `SettingsPage` using `TabLayout` and `TabPage` components.

--- src/views/settings/SettingsPage.vue
+++ src/views/settings/SettingsPage.vue
@@ -2,12 +2,19 @@
 
 import PageLayout from "../../components/PageLayout.vue";
 import PageHeader from "../../components/PageHeader.vue";
+import TabLayout from "../../components/TabLayout.vue";
+import TabPage from "../../components/TabPage.vue";
+import PatsTab from "./PatsTab.vue";
 </script>
 
 <template>
   <PageLayout>
     <PageHeader title="Settings" subtitle="Configure your personal dockyard settings."/>
-    TODO
+    <TabLayout>
+      <TabPage title="PATs" name="pats">
+        <PatsTab/>
+      </TabPage>
+    </TabLayout>
   </PageLayout>
 </template>
 
